6607c68b6730be1070bdfc5abac6f303fdc241fc
Filip <43730405+Gecazo@users.noreply.github.com>
2021-09-08 12:26:30 +0300

    Delay for field generation created

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ MAX_SIZE = 10
 DEAD_CELL = " "
 LIVE_CELL = "*"
 
+def show_field(field):
+    for y in range(MAX_SIZE):
+        print(''.join(field[y]))
+
 # Generation
 field = [
     [choice([DEAD_CELL, LIVE_CELL]) for x in range(MAX_SIZE)] for y in range(MAX_SIZE)
 ]
 
-print(field)
+# Pause for next move
+while True:
+    input('press any key for next step')
+    show_field(field)
